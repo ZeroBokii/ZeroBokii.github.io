011ca6d30b63773bc867aaf7de08a8b1474404ae
Torch <Torch@gmail.com>
2024-09-07 21:52:10 +0800

    Update Hexo Static Content

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2024/09/07/hello-world/"];function toRandomPost(){
+var posts=["2024/09/07/hello-world/","2024/09/07/生产者消费者模式/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
